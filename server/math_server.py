d4d2d2d3c86973437f637ddb1dc7c37d88e4b68b
ThangDQ <quangthang260602@gmail.com>
2025-05-22 11:25:45 +0700

    update project

--- server/math_server.py
+++ server/math_server.py
@@ -1,40 +1,41 @@
 # math_server.py
 from mcp.server.fastmcp import FastMCP
+from typing import Literal, Union
 
 mcp = FastMCP("Math")
 
 @mcp.tool()
-def add(a: int, b: int) -> int:
+def add(a: Union[float, int], b: Union[float, int]) -> Union[float, int]:
     """Add two numbers"""
     return a + b
 
 @mcp.tool()
-def multiply(a: int, b: int) -> int:
+def multiply(a: Union[float, int], b: Union[float, int]) -> Union[float, int]:
     """Multiply two numbers"""
     return a * b
 
 @mcp.tool()
-def subtract(a: int, b: int) -> int:
+def subtract(a: Union[float, int], b: Union[float, int]) -> Union[float, int]:
     """Subtract two numbers"""
     return a - b
 
 @mcp.tool()
-def divide(a: int, b: int) -> int:
+def divide(a: Union[float, int], b: Union[float, int]) -> Union[float, int]:
     """Divide two numbers"""
     return a / b
 
 @mcp.tool()
-def power(a: int, b: int) -> int:
+def power(a: Union[float, int], b: Union[float, int]) -> Union[float, int]:
     """Raise a to the power of b"""
     return a ** b
 
 @mcp.tool()
-def sqrt(a: int) -> int:
+def sqrt(a: Union[float, int]) -> Union[float, int]:
     """Square root of a"""
     return a ** 0.5
 
 @mcp.tool()
-def cube(a: int) -> int:
+def cube(a: Union[float, int]) -> Union[float, int]:
     """Cube of a"""
     return a ** 3
 
